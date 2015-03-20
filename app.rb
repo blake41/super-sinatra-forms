f8749078bd5d5e6d52848693616e5816bd5c8c63
Blake Johnson <blake41@gmail.com>
2015-03-20 15:37:04 -0400

    done

--- app.rb
+++ app.rb
@@ -2,4 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/" do
+    erb :super_hero
+  end
+
+  post "/teams" do
+    # binding.pry
+    # Team.create(:name => params["team"]["name"], :motto => params["team"]["motto"])
+    # params["team"]["members"].each do |member_id, member|
+    #   Member.create(member)
+    # end
+    erb :team
+  end
+
 end
